dcd9ac84a24e003b7406b927bcf5f482e9e055e8
Sergiy <shkum79@ukr.net>
2022-06-11 20:03:31 +0200

    Add project files.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Tric = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Mnu1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem4 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Mnu1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 68)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(106, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Select folder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(223, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Following folder will be monitored for new files:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label2.Location = New System.Drawing.Point(13, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "----"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(16, 130)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(137, 17)
+        Me.CheckBox1.TabIndex = 3
+        Me.CheckBox1.Text = "Autostart with Windows"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(151, 68)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(106, 27)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Start monitoring"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Tric
+        '
+        Me.Tric.ContextMenuStrip = Me.Mnu1
+        Me.Tric.Icon = CType(resources.GetObject("Tric.Icon"), System.Drawing.Icon)
+        Me.Tric.Text = "No new files!"
+        Me.Tric.Visible = True
+        '
+        'Mnu1
+        '
+        Me.Mnu1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.ToolStripMenuItem2, Me.ToolStripMenuItem3, Me.ToolStripMenuItem4, Me.ExitToolStripMenuItem})
+        Me.Mnu1.Name = "ContextMenuStrip1"
+        Me.Mnu1.Size = New System.Drawing.Size(162, 114)
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.ToolStripMenuItem1.Image = CType(resources.GetObject("ToolStripMenuItem1.Image"), System.Drawing.Image)
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(161, 22)
+        Me.ToolStripMenuItem1.Text = "Setting window"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Enabled = False
+        Me.ToolStripMenuItem2.Image = CType(resources.GetObject("ToolStripMenuItem2.Image"), System.Drawing.Image)
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(161, 22)
+        Me.ToolStripMenuItem2.Text = "Open folder"
+        '
+        'ToolStripMenuItem3
+        '
+        Me.ToolStripMenuItem3.Enabled = False
+        Me.ToolStripMenuItem3.Image = CType(resources.GetObject("ToolStripMenuItem3.Image"), System.Drawing.Image)
+        Me.ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        Me.ToolStripMenuItem3.Size = New System.Drawing.Size(161, 22)
+        Me.ToolStripMenuItem3.Text = "Start Monitoring"
+        '
+        'ToolStripMenuItem4
+        '
+        Me.ToolStripMenuItem4.Image = CType(resources.GetObject("ToolStripMenuItem4.Image"), System.Drawing.Image)
+        Me.ToolStripMenuItem4.Name = "ToolStripMenuItem4"
+        Me.ToolStripMenuItem4.Size = New System.Drawing.Size(161, 22)
+        Me.ToolStripMenuItem4.Text = "About"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Image = CType(resources.GetObject("ExitToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(161, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 400
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(286, 68)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(106, 27)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Hide"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(286, 119)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(106, 28)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Exit"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(404, 159)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Monitoring stoped ..."
+        Me.Mnu1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Tric As NotifyIcon
+    Friend WithEvents Mnu1 As ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ToolStripMenuItem4 As ToolStripMenuItem
+    Friend WithEvents Button4 As Button
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,236 @@
+﻿Imports System.ComponentModel
+Imports Microsoft.Win32
+Public Class Form1
+    Dim icoCnt As Integer
+    Dim oldF As Long
+    Dim newF As Long
+    Dim aFlag As Boolean
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        Try
+            Dim regStart As RegistryKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+            If CheckBox1.Checked = True Then
+                regStart.SetValue("NewFileMonitor", """" & Application.ExecutablePath & """" & " -Start")
+            Else
+                regStart.DeleteValue("NewFileMonitor")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message & vbCrLf & "Restart program as Administrator", vbInformation)
+        End Try
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If aFlag Then
+            If MsgBox("Are you sure to stop monitoring and select new folder?", vbQuestion + vbYesNo, "Select new folder for monitoring?") = vbNo Then Exit Sub
+        End If
+        If aFlag = True Then
+            aFlag = False
+            ToolStripMenuItem2.Enabled = False
+            ToolStripMenuItem3.Enabled = False
+            Button2.Text = "Start monitoring"
+            ToolStripMenuItem3.Text = "Start monitoring"
+            Me.Text = "Monitoring stopped ..."
+        End If
+        Dim a As FolderBrowserDialog = New FolderBrowserDialog
+        Dim s As String
+        a.Description = "Select folder for monitoring"
+        a.ShowDialog()
+        s = a.SelectedPath
+        If s <> "" Then
+            Label2.Text = s
+            Button2.Enabled = True
+            ToolStripMenuItem2.Enabled = True
+            ToolStripMenuItem3.Enabled = True
+            SaveSetting("FolderMonitor", "Settings", "FolderName", s)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run").GetValue("NewFileMonitor") <> "" Then CheckBox1.Checked = True
+        'If GetSetting("FolderMonitor", "Settings", "CheckFolder") = "" Then SaveSetting("FolderMonitor", "Settings", "CheckFolder", CheckBox2.Checked.ToString)
+        ' CheckBox2.Checked = GetSetting("FolderMonitor", "Settings", "CheckFolder")
+        If IO.Directory.Exists(GetSetting("FolderMonitor", "Settings", "FolderName")) Then
+            Label2.Text = GetSetting("FolderMonitor", "Settings", "FolderName")
+            Dim enbl As Boolean = GetSetting("FolderMonitor", "Settings", "FolderName") <> ""
+            Button2.Enabled = enbl
+            ToolStripMenuItem2.Enabled = enbl
+            ToolStripMenuItem3.Enabled = enbl
+            If Command() = "-Start" And Label2.Text <> "" Then
+                Me.Visible = False
+                Me.ShowInTaskbar = False
+                Me.WindowState = FormWindowState.Minimized
+
+            End If
+        ElseIf (GetSetting("FolderMonitor", "Settings", "FolderName") <> "") And (IO.Directory.Exists(GetSetting("FolderMonitor", "Settings", "FolderName")) = False) Then
+            MsgBox("Folder not selected or wrong path.", vbInformation)
+            SaveSetting("FolderMonitor", "Settings", "FolderName", "")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If IO.Directory.Exists(Label2.Text) = False Then
+            MsgBox("Folder selected for monitoring not available", vbInformation)
+            aFlag = False
+            ToolStripMenuItem2.Enabled = False
+            ToolStripMenuItem3.Enabled = False
+            Button2.Text = "Start monitoring"
+            ToolStripMenuItem3.Text = "Start monitoring"
+            Me.Text = "Monitoring stopped ..."
+            Exit Sub
+        End If
+        aFlag = Not aFlag
+        Button2.Text = If(aFlag = False, "Start monitoring", "Stop monitoring")
+        ToolStripMenuItem3.Text = If(aFlag = False, "Start monitoring", "Stop monitoring")
+        Me.Text = If(aFlag = False, "Monitoring stopped ...", "Monitoring in progress ...")
+        If aFlag = True Then
+            ToolStripMenuItem2.Enabled = True
+            ToolStripMenuItem3.Enabled = True
+            StartMon()
+        End If
+    End Sub
+
+    Private Sub StartMon()
+        Me.Hide()
+        oldF = System.IO.Directory.GetFiles(Label2.Text).Length
+        Dim t As New Threading.Thread(AddressOf Monitoring) With {.Priority = Threading.ThreadPriority.Lowest}
+        t.Start()
+    End Sub
+
+    Private Sub Monitoring()
+        Do While (aFlag = True)
+            If IO.Directory.Exists(Label2.Text) Then
+
+                newF = System.IO.Directory.GetFiles(Label2.Text).Length
+                If newF > oldF Then
+                    Tric.BalloonTipText = "New file detected in your folder..."
+                    Tric.Text = "New file detected..."
+                    Tric.ShowBalloonTip(5000)
+                    Timer1.Enabled = True
+                    oldF = newF
+                End If
+                If newF < oldF Then oldF = newF
+
+            End If
+            Application.DoEvents()
+            System.Threading.Thread.Sleep(500)
+        Loop
+    End Sub
+
+
+    Private Sub Tric_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Tric.MouseDoubleClick
+
+        If e.Button = MouseButtons.Left Then
+            Me.Visible = False
+            Interaction.Shell("explorer " & Label2.Text, AppWinStyle.MaximizedFocus)
+        End If
+
+    End Sub
+
+    Private Sub Tric_MouseClick(sender As Object, e As MouseEventArgs) Handles Tric.MouseClick
+        If e.Button = MouseButtons.Right Then
+            Tric.ContextMenuStrip.Show()
+        End If
+        If e.Button = MouseButtons.Left Then
+            If IO.Directory.Exists(GetSetting("FolderMonitor", "Settings", "FolderName")) Then
+                oldF = System.IO.Directory.GetFiles(Label2.Text).Length
+                If Timer1.Enabled = True Then
+                    Interaction.Shell("explorer " & Label2.Text, AppWinStyle.MaximizedFocus)
+                Else
+                    If e.Button = MouseButtons.Left Then Me.Visible = Not Me.Visible : Me.WindowState = vbNormal
+                End If
+                Timer1.Enabled = False
+                Tric.BalloonTipText = "No new files detected in your folder..."
+                Tric.Text = "No new files!"
+                Tric.Icon = My.Resources.fire
+            End If
+        End If
+
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        Me.Show()
+    End Sub
+
+    Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem2.Click
+        If Label2.Text <> "" Then Interaction.Shell("explorer " & Label2.Text, AppWinStyle.MaximizedFocus)
+    End Sub
+
+    Private Sub ToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem3.Click
+
+        If IO.Directory.Exists(Label2.Text) = False Then
+            MsgBox("Folder selected for monitoring not available", vbInformation)
+            aFlag = False
+            ToolStripMenuItem2.Enabled = False
+            ToolStripMenuItem3.Enabled = False
+            Button2.Text = "Start monitoring"
+            ToolStripMenuItem3.Text = "Start monitoring"
+            Me.Text = "Monitoring stopped ..."
+            Exit Sub
+        End If
+
+        aFlag = Not aFlag
+        Button2.Text = If(aFlag = False, "Start monitoring", "Stop Monitoring")
+        ToolStripMenuItem3.Text = If(aFlag = False, "Start monitoring", "Stop monitoring")
+        If aFlag = True Then StartMon()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        If aFlag Then
+            If MsgBox("Are you sure to stop monitoring and exit?", vbQuestion + vbYesNo, "Exit program?") = vbYes Then
+                aFlag = False
+                Me.Close()
+            End If
+        Else
+            aFlag = False
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        icoCnt += 1
+        Tric.Icon = If(icoCnt = 1, My.Resources._1, If(icoCnt = 2, My.Resources._2, My.Resources._3))
+        If icoCnt = 3 Then icoCnt = 0
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        If aFlag Then
+            If MsgBox("Are you sure to stop monitoring and exit?", vbQuestion + vbYesNo, "Exit program?") = vbYes Then
+                aFlag = False
+            Else e.Cancel = True
+            End If
+        Else
+            aFlag = False
+        End If
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        If Command() = "-Start" And Label2.Text <> "" Then
+            Button2.PerformClick()
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Hide()
+    End Sub
+
+    Private Sub ToolStripMenuItem4_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem4.Click
+        MsgBox("This super-puper-mega-hyper-cool program" & vbCrLf & "made by Sergiy Shkumat", vbInformation, "About this software...")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If aFlag Then
+            If MsgBox("Are you sure to stop monitoring and exit?", vbQuestion + vbYesNo, "Exit program?") = vbYes Then
+                aFlag = False
+                Me.Close()
+            End If
+        Else
+            aFlag = False
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Mnu1_LostFocus(sender As Object, e As EventArgs) Handles Mnu1.LostFocus
+        Tric.ShowBalloonTip(False)
+    End Sub
+End Class
